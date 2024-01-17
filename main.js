bf508a7411b6e389f4ac353f3aeab8a8fc7526fe
Geofrey K imani <jeffkimzyjoe@gmail.com>
2024-01-17 06:35:32 +0300

    Applying logic to higher order functions

--- main.js
+++ main.js
@@ -1,226 +1,249 @@
-//Nested Functions
-let a = 10
-function outer() {
-    let b = 20
-    function inner() {
-        let c = 30
-        console.log(a,b,c)
-    }
-    inner()
-}
-outer()
+// //Nested Functions
+// let a = 10
+// function outer() {
+//     let b = 20
+//     function inner() {
+//         let c = 30
+//         console.log(a,b,c)
+//     }
+//     inner()
+// }
+// outer()
 
-//Closures in Javascript
-function outer () {
-    let a = 10
-    function inner () {
-        a++
-        console.log(a)
-    }
-    return inner
-}
-const fn = outer()
-console.log(fn)
-fn()
-fn()
+// //Closures in Javascript
+// function outer () {
+//     let a = 10
+//     function inner () {
+//         a++
+//         console.log(a)
+//     }
+//     return inner
+// }
+// const fn = outer()
+// console.log(fn)
+// fn()
+// fn()
 
-//Currying 
-//Changing -> sum (2,3,5) to sum(2)(3)(5)
-function sum (a,b,c) {
-    return a+b+c
-}
+// //Currying 
+// //Changing -> sum (2,3,5) to sum(2)(3)(5)
+// function sum (a,b,c) {
+//     return a+b+c
+// }
 
-function curry(fn) {
-    return function (a) {
-        return function(b) {
-            return function(c) {
-                return fn (a,b,c)
-            }
-        }
-    }
-}
+// function curry(fn) {
+//     return function (a) {
+//         return function(b) {
+//             return function(c) {
+//                 return fn (a,b,c)
+//             }
+//         }
+//     }
+// }
 
-const curriedSum = curry(sum)
+// const curriedSum = curry(sum)
 
-const add2 = curriedSum(2)
-const add3 = add2(3)
-const add5 = add3(5)
-console.log(add5)
+// const add2 = curriedSum(2)
+// const add3 = add2(3)
+// const add5 = add3(5)
+// console.log(add5)
 
-//Types of binding in Javascript
-function sayMyName (name) { //name is a parameter
-    console.log(`My name is ${name}`) //name rendered dynamically
-} 
+// //Types of binding in Javascript
+// function sayMyName (name) { //name is a parameter
+//     console.log(`My name is ${name}`) //name rendered dynamically
+// } 
 
-sayMyName("Geoffrey") //Geoffrey is an argument in calling sayMyName function
-sayMyName("Kimani")
-
-
-//impilicit binding
-const Person = {
-    name: "Geofrey",
-    sayMyName:function(name) {
-        console.log(`My name is ${this.name}`)
-    }
-}
-Person.sayMyName()
+// sayMyName("Geoffrey") //Geoffrey is an argument in calling sayMyName function
+// sayMyName("Kimani")
 
 
-const name = "superman"
-globalThis.name = "superman"
-
-function sayMyName () {
-    console.log(`my name is ${this.name}`)
-}
-
-sayMyName.call(Person) 
-
-//The this key word
-function Person (name) {
-    this.name = name
-}
-
-//The new key word
-const P1 = new Person ("Geoffrey")
-const P2 = new Person("Kimani")
-
-console.log(P1.name,P2.name)
-
-sayMyName()
+// //impilicit binding
+// const Person = {
+//     name: "Geofrey",
+//     sayMyName:function(name) {
+//         console.log(`My name is ${this.name}`)
+//     }
+// }
+// Person.sayMyName()
 
 
-//Prototype
-function Person (fName,lName) {
-    this.fName = fName
-    this.lName = lName
-}
+// const name = "superman"
+// globalThis.name = "superman"
 
-//inheriting methods using prototypes and functions
-function SuperHero(fName, lName) {
-    Person.call(this, fName,lName)
-    this.isSuperHero = true
-}
+// function sayMyName () {
+//     console.log(`my name is ${this.name}`)
+// }
 
-Person.prototype.getfullname = function() {
-    return (`${this.fName} ${this.lName}`)
-}
+// sayMyName.call(Person) 
 
-SuperHero.prototype.fightCrime = function () {
-    console.log("Fighting crime")
-} 
+// //The this key word
+// function Person (name) {
+//     this.name = name
+// }
 
-SuperHero.prototype = Object.create(Person.prototype)
+// //The new key word
+// const P1 = new Person ("Geoffrey")
+// const P2 = new Person("Kimani")
 
-const batman = new SuperHero("Bruce", "Wayne")
+// console.log(P1.name,P2.name)
 
-SuperHero.prototype.constructor = SuperHero
+// sayMyName()
 
-console.log(batman.getfullname())
 
-//class syntactical sugar
-class Person {
-    constructor(fName,lName) {
-        this.firstName = fName
-        this.lastName = lName
-    }
-    sayMyName() {
-        return (`${this.firstName} ${this.lastName}`)
-    }
-}
+// //Prototype
+// function Person (fName,lName) {
+//     this.fName = fName
+//     this.lName = lName
+// }
 
-const person1 = new Person ("Geofrey", "Kimani")
-console.log(person1.sayMyName())
+// //inheriting methods using prototypes and functions
+// function SuperHero(fName, lName) {
+//     Person.call(this, fName,lName)
+//     this.isSuperHero = true
+// }
 
-class SuperHero extends Person {
-    constructor (fName,lName) {
-        super(fName,lName)
-        this.isSuperHero = true
-    }
-    fightCrime() {
-        console.log("fighting crime")
-    }
-}
+// Person.prototype.getfullname = function() {
+//     return (`${this.fName} ${this.lName}`)
+// }
 
-const battman = new SuperHero ("Bruce", "Wayne")
-console.log(battman.sayMyName())
+// SuperHero.prototype.fightCrime = function () {
+//     console.log("Fighting crime")
+// } 
 
-//For loop
-const str = "Geoffrey"
-for (i=0;i<str.length;i++) {
-    console.log(str.charAt(i))
-}
+// SuperHero.prototype = Object.create(Person.prototype)
 
-const arr = ["G","e","o","f","f","r","e","y"]
+// const batman = new SuperHero("Bruce", "Wayne")
 
-for (i=0;i<arr.length;i++) {
-    console.log(arr[i])
-}
+// SuperHero.prototype.constructor = SuperHero
 
-//for of loop
-const stry = "Kimani"
-for(const char of stry) {
-    console.log(char)
-}
+// console.log(batman.getfullname())
 
-const arry = ["G","e","o","f","f","r","e","y"]
-for (item of arry) {
-    console.log(item)
-}
+// //class syntactical sugar
+// class Person {
+//     constructor(fName,lName) {
+//         this.firstName = fName
+//         this.lastName = lName
+//     }
+//     sayMyName() {
+//         return (`${this.firstName} ${this.lastName}`)
+//     }
+// }
 
-//creating an iterable object in JavaScript using Symbol.iterator. 
+// const person1 = new Person ("Geofrey", "Kimani")
+// console.log(person1.sayMyName())
 
-const obj = {
-    [Symbol.iterator]: function() {
-        let step = 0
-        const iterator = {
-            next: function() {
-                step++
-                if(step === 1) {
-                    return {value:"Hello", done:false}
-                } else if (step === 2) {
-                    return {value:"world", done:false}
-                }
-                return {value:undefined, done:true}
-            }
-        }
-        return iterator
-    }
-}
+// class SuperHero extends Person {
+//     constructor (fName,lName) {
+//         super(fName,lName)
+//         this.isSuperHero = true
+//     }
+//     fightCrime() {
+//         console.log("fighting crime")
+//     }
+// }
 
-for (const word of obj) {
-    console.log(word)
-}
+// const battman = new SuperHero ("Bruce", "Wayne")
+// console.log(battman.sayMyName())
 
-function normalFunction () {
-    console.log("Hello")
-    console.log("World")
-}
+// //For loop
+// const str = "Geoffrey"
+// for (i=0;i<str.length;i++) {
+//     console.log(str.charAt(i))
+// }
 
-normalFunction()
-normalFunction()
+// const arr = ["G","e","o","f","f","r","e","y"]
+
+// for (i=0;i<arr.length;i++) {
+//     console.log(arr[i])
+// }
+
+// //for of loop
+// const stry = "Kimani"
+// for(const char of stry) {
+//     console.log(char)
+// }
+
+// const arry = ["G","e","o","f","f","r","e","y"]
+// for (item of arry) {
+//     console.log(item)
+// }
+
+// //creating an iterable object in JavaScript using Symbol.iterator. 
+
+// const obj = {
+//     [Symbol.iterator]: function() {
+//         let step = 0
+//         const iterator = {
+//             next: function() {
+//                 step++
+//                 if(step === 1) {
+//                     return {value:"Hello", done:false}
+//                 } else if (step === 2) {
+//                     return {value:"world", done:false}
+//                 }
+//                 return {value:undefined, done:true}
+//             }
+//         }
+//         return iterator
+//     }
+// }
+
+// for (const word of obj) {
+//     console.log(word)
+// }
+
+// function normalFunction () {
+//     console.log("Hello")
+//     console.log("World")
+// }
+
+// normalFunction()
+// normalFunction()
 
 //Generator function
-function* generatorFunction() {
-    yield "Hello"
-    yield "world"
+// function* generatorFunction() {
+//     yield "Hello"
+//     yield "world"
+// }
+
+// const generatorObject = generatorFunction()
+//  for(const i of generatorObject) {
+//     console.log(i)
+//  } 
+
+// //.map() method .filter refer to code with mosh on yt
+// const numbers = [1,-1,2,3]
+
+// const items = numbers
+// .filter(n=> n >= 0)
+// .map((n) => {
+//     return {value:n}
+// })
+
+// console.log(items)
+
+
+//Applying logic to higher order functions
+
+const radius = [3,1,2,4]
+
+const area = function (radius) {
+    return Math.PI * radius * radius
 }
 
-const generatorObject = generatorFunction()
- for(const i of generatorObject) {
-    console.log(i)
- }
+const circumference = function (radius) {
+    return 2 * Math.PI * radius
+}
 
-//.map() method .filter refer to code with mosh on yt
-const numbers = [1,-1,2,3]
+const calculate = function (radius, logic) {
+    const output = []
+    for (let i = 0;i<radius.length;i++) {
+        output.push(logic(radius[i]))
+    }
+    return output
+}
 
-const items = numbers
-.filter(n=> n >= 0)
-.map((n) => {
-    return {value:n}
-})
-
-console.log(items)
-
+console.log (calculate(radius,circumference))
+console.log (calculate(radius,area))
 
 
 
